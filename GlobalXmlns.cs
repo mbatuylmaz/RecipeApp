fb51bd64596510034cba866b2157a18d40029436
Batuhan Yılmaz <101661466+mbatuylmaz@users.noreply.github.com>
2026-01-06 16:43:31 +0300

    Proje baslatildi

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "RecipeApp")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "RecipeApp.Pages")]
